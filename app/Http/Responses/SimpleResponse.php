be3122ac71184e146644e76ec9515a028920b995
Jeffsitorus <jefrisitorus22@gmail.com>
2021-10-09 17:49:59 +0700

    make function response & handling errors

---
+++ app/Http/Responses/SimpleResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Responses;
+
+class SimpleResponse extends BaseResponse
+{
+    public function __construct($data, $message)
+    {
+        if (is_null($message)) {
+            parent::__construct(200,  $data);
+        } else {
+            parent::__construct(200, $data, $message);
+        }
+    }
+}
